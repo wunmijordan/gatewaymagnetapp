ef6ea0d04e87fc614bc19a3f75cb6c022546132b
Wunmi <wunmijordan@gmail.com>
2025-07-25 06:59:28 +0100

    Cloudinary Uploader

--- gatewaymagnetapp/settings.py
+++ gatewaymagnetapp/settings.py
@@ -15,6 +15,8 @@ import os
 import dj_database_url
 import cloudinary
 import cloudinary_storage
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
