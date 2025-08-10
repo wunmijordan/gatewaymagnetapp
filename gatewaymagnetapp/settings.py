7ce73771c7a7e772dbfa1a02a94ea8b9e16762af
Wunmi <wunmijordan@gmail.com>
2025-08-10 04:52:51 +0100

    Modified settings.py

--- gatewaymagnetapp/settings.py
+++ gatewaymagnetapp/settings.py
@@ -168,7 +168,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static and media files
-STATIC_URL = '/static/'
+STATIC_URL = 'https://gatewaymagnetapp-static.onrender.com/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
