4baa5045f73e80ef3fea3e0c4bb61375a75b459a
Wunmi <wunmijordan@gmail.com>
2025-08-10 06:37:17 +0100

    Modified Guest page and Pictures upload

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 def user_profile_upload_path(instance, filename):
     return f'profile_pics/{instance.user.username}/{filename}'
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to=user_profile_upload_path, blank=True, null=True)
+    image = CloudinaryField('image', blank=True, null=True)
 
     def __str__(self):
         return self.user.username
